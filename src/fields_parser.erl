f9575828d049ccce8ff99dbc88a3b8124ed2b9e3
Erik Reitsma <development@ernovation.com>
2017-10-31 21:51:34 +0100

    Add specification of FADT.

--- src/fields_parser.erl
+++ src/fields_parser.erl
@@ -10,6 +10,9 @@ parse_fields([], Rest, Res) ->
     {Res, [], Rest};
 parse_fields(Specs, <<>>, Res) ->
     {Res, Specs, <<>>};
+parse_fields([{unused, Spec} | More], Data, Res) ->
+    {_Value, MoreData} = parse_field(Spec, Data),
+    parse_fields(More, MoreData, Res);
 parse_fields([{Name, Spec} | More], Data, Res) ->
     {Value, MoreData} = parse_field(Spec, Data),
     parse_fields(More, MoreData, Res#{Name => Value}).
@@ -20,7 +23,12 @@ parse_field(word, <<W:16/little, More/binary>>) ->
     {W, More};
 parse_field(dword, <<DW:32/little, More/binary>>) ->
     {DW, More};
-parse_field(dword, <<QW:64/little, More/binary>>) ->
+parse_field(qword, <<QW:64/little, More/binary>>) ->
     {QW, More};
+parse_field({binary, N}, Data) ->
+    <<B:N/binary, More/binary>> = Data,
+    {B, More};
+parse_field(binary_sized, <<Size, B:Size/binary, More/binary>>) ->
+    {B, More};
 parse_field(_Other, Data) ->
     {{todo, Data}, <<>>}.
